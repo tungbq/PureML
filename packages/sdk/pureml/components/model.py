7f03d3d0d816501a9fb41c4094eeb288513c5614
vamsidhar muthireddy <vamsi.muthireddy@gmail.com>
2023-02-03 01:58:45 +0530

    Close T-1101 Close T-1093 model component is functional for go backend

--- packages/sdk/pureml/components/model.py
+++ packages/sdk/pureml/components/model.py
@@ -424,7 +424,7 @@ def fetch(name: str, branch: str, version: str = "latest"):
 
     model_url = urljoin(storage_source_type, storage_path)
     
-    print(model_url)
+    # print(model_url)
 
     headers = {
         "Content-Type": "application/x-www-form-urlencoded",
