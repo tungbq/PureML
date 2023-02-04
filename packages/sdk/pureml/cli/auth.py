98297db7c588cfcd98e57088f0f75e398ab33f2e
vamsidhar muthireddy <vamsi.muthireddy@gmail.com>
2023-02-04 16:35:00 +0530

    links T-1426 Close T-1426 Diplays the list of available organizations to the user during login using cli

--- packages/sdk/pureml/cli/auth.py
+++ packages/sdk/pureml/cli/auth.py
@@ -8,7 +8,8 @@ from rich import print
 from rich.syntax import Syntax
 from pureml.utils.constants import BASE_URL, PATH_USER_TOKEN
 from pureml.components import get_org_id, get_token
-
+from rich.console import Console
+from rich.table import Table
 from urllib.parse import urljoin
 import json
 
@@ -65,10 +66,12 @@ def callback():
 def signup():
     print("\n[bold]Create a new account[/bold]\n")
     email: str = typer.prompt("Enter new email")
+    handle: str = typer.prompt("Enter new user handle")
+    name: str = typer.prompt("Enter new user name")
     password: str = typer.prompt("Enter new password", confirmation_prompt=True, hide_input=True)
     # organization_id: str = typer.prompt("Enter Organization id")
     # data = {"email": email, "password": password, "org": organization_id}
-    data = {"email": email, "password": password}
+    data = {"email": email, "password": password, "handle": handle, "name":name}
 
 
     url_path_1 = 'user/signup'
@@ -81,6 +84,39 @@ def signup():
         print(f"[bold red]Could not create account! Please try again later")
         return
     print(f"[bold green]Successfully created your account! You can now login using ```pure auth login```")
+
+
+def list_org(access_token):
+
+    url_path = 'org/'
+    url = urljoin(BASE_URL, url_path)
+
+    headers = {
+        'accept': 'application/json',
+        'Authorization': 'Bearer {}'.format(access_token)
+    }
+
+
+    response = requests.get(url,headers=headers)
+
+    if response.ok:
+        print()
+        print('[bold green]Select the Organization from the list below!')
+        org_all = response.json()['data']
+
+        console = Console()
+
+        table = Table( "User Handle", "Organization Id")
+        for org in org_all:
+            table.add_row(org['org']['handle'], org['org']['uuid'])
+    
+        console.print(table)
+        print()
+
+
+    else:
+        print('[bold red]Unable to fetch existing Organizations!')
+
 
 
 
@@ -126,10 +162,12 @@ def login():
     
     if response.ok:
         token = response.text
-        token = json.loads(token)['Data'][0]
+        token = json.loads(token)['data'][0]
 
         access_token = token['accessToken']
         email = token['email']
+
+        list_org(access_token=access_token)
 
         org_id = check_org_status(access_token=access_token)
 
