d050d8146398bd97dc746640dae28ac715b1259d
vamsidhar muthireddy <vamsi.muthireddy@gmail.com>
2023-02-27 03:17:56 +0530

    Added og schema

--- packages/sdk/pureml/schema/log.py
+++ packages/sdk/pureml/schema/log.py
@@ -7,6 +7,3 @@ import os
 class LogSchema(BaseModel):
     paths: PathSchema = PathSchema().get_instance()
     backend: BackendSchema = BackendSchema().get_instance()
-
-    PATH_MODEL_README: str = os.path.join(paths.PATH_MODEL_DIR, "ReadME.md")
-    PATH_MODEL: str = os.path.join(paths.PATH_MODEL_DIR, "ran.pkl")
