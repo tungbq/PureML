6f582e092aadabc3ab197cabf23e15008e027834
vamsidhar muthireddy <vamsi.muthireddy@gmail.com>
2023-03-15 04:34:14 +0530

    Reverted back metadata to packaging

--- packages/sdk/pureml/packaging/__init__.py
+++ packages/sdk/pureml/packaging/__init__.py
@@ -1,20 +1,19 @@
 import os
 from pathlib import Path
-from .packaging import Model
+from .metadata import Model
 
 
+def save_model(model, model_name, model_path) -> None:
+    """The function takes in a model and a model name, and saves the model to the default `models` directory.
 
-def save_model(model, model_name, model_path) -> None: 
-    ''' The function takes in a model and a model name, and saves the model to the default `models` directory. 
-    
     Parameters
     ----------
     model
         The model you want to save.
     model_name
         The name of the model.
-    
-    '''
+
+    """
     model_dir = os.path.dirname(model_path)
     # print(model_dir)
     # os.makedirs(model_dir, exist_ok=True)
@@ -22,21 +21,19 @@ def save_model(model, model_name, model_path) -> None:
     Model(model=model, model_name=model_name, model_path=model_path).save_model()
 
 
+def load_model(model_path: Path):
+    """Loads a model from a given path
 
-def load_model(model_path:Path):
-    ''' Loads a model from a given path
-    
     Parameters
     ----------
     model_path : Path
         The path to the model you want to load.
-    
+
     Returns
     -------
         The model is being returned.
-    
-    '''
+
+    """
     model = Model(model_path=model_path).load_model()
 
     return model
-
