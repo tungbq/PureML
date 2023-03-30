3a8c836caea79401aeafb1c83d244ad2190602ff
Priyav Kaneria <priyavkaneria@gmail.com>
2023-03-30 23:21:30 +0530

    SDK cli secrets support

--- packages/sdk/pureml/schema/backend.py
+++ packages/sdk/pureml/schema/backend.py
@@ -7,6 +7,27 @@ import os
 class BackendSchema(Singleton_BaseModel):
 
     BASE_URL: str = "https://pureml-development.up.railway.app/api/"
+    INTEGRATIONS: dict = {
+        "s3": {
+            "name": "AWS S3 Object Storage",
+            "secrets": [
+                "access_key_id",
+                "access_key_secret",
+                "bucket_location",
+                "bucket_name",
+            ],
+        },
+        "r2": {
+            "name": "Cloudflare R2 Object Storage",
+            "secrets": [
+                "access_key_id",
+                "access_key_secret",
+                "account_id",
+                "bucket_name",
+                "public_url",
+            ],
+        }
+    }
 
     class Config:
         arbitrary_types_allowed = True
